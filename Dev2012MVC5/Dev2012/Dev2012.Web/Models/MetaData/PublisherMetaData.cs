9b87778b9683894c9c819e87f106fa006a7f7267
EagleEyes <qquocviet2602@gmail.com>
2021-03-03 21:20:27 +0700

    Session05

---
+++ Dev2012MVC5/Dev2012/Dev2012.Web/Models/MetaData/PublisherMetaData.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Dev2012.Web.Models.MetaData
+{
+    [MetadataType(typeof(Publisher.Metadata))]
+    public partial class Publisher
+    {
+        private sealed class Metadata
+        {
+            [Required]
+            [StringLength(15, MinimumLength = 8, ErrorMessage = "Mã không được nhỏ hơn 8 ký tự và lớn hơn 15 ký tự")]
+            [DisplayName("Mã NXB")]
+            public string Code { get; set; }
+
+            [Required(ErrorMessage = "Tên không được để trống")]
+            [DisplayName("Tên NXB")]
+            public string Name { get; set; }
+
+            [DisplayName("Chú thích")]
+            public string Notes { get; set; }
+
+            [Required]
+            [Range(0, 1, ErrorMessage = "Trạng thái chỉ có thể là 0 hoặc 1")]
+            [DisplayName("Trạng thái")]
+            public Nullable<byte> Status { get; set; }
+
+        }
+    }
+}
--- Dev2012MVC5/Dev2012/Dev2012.Web/Models/Publisher.cs
+++ Dev2012MVC5/Dev2012/Dev2012.Web/Models/Publisher.cs
@@ -17,21 +17,13 @@ namespace Dev2012.Web.Models
     public partial class Publisher
     {
         public int Id { get; set; }
-        [Required]
-        [StringLength(15, MinimumLength = 8, ErrorMessage = "Mã không được nhỏ hơn 8 ký tự và lớn hơn 15 ký tự")]
-        [DisplayName("Mã NXB")]
+
         public string Code { get; set; }
 
-        [Required(ErrorMessage = "Tên không được để trống")]
-        [DisplayName("Tên NXB")]
         public string Name { get; set; }
 
-        [DisplayName("Chú thích")]
         public string Notes { get; set; }
 
-        [Required]
-        [Range(0, 1, ErrorMessage = "Trạng thái chỉ có thể là 0 hoặc 1")]
-        [DisplayName("Trạng thái")]
         public Nullable<byte> Status { get; set; }
 
         public Nullable<bool> IsDelete { get; set; }
